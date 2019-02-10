017c2d077dd9e2ba32c25bd4e0b5c79bc354ab47
Admin <admin@Yashs-MacBook-Air.local>
2019-02-10 15:34:22 +0530

    Add new Items and Checked,Unchecked functionality added

--- Try/ViewController.m
+++ Try/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIAlertViewDelegate>
 @property (nonatomic) NSMutableArray *items;
 
 @end
@@ -18,10 +18,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.items = @[@"item1",@"item2"].mutableCopy;
+    self.items = @[@{@"name":@"item1"},@{@"name":@"item2"}].mutableCopy;
     self.navigationItem.title=@"To-Do-List";
     NSLog(@"viewdidload");
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewitem:)];
 
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
+    NSMutableDictionary *item = [self.items[indexPath.row] mutableCopy];
+    BOOL completed = [item[@"completed"] boolValue];
+    NSLog(@"Completed: %d",completed);
+    item[@"completed"] = @(!completed);
+    self.items[indexPath.row] = item;
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType = ([item[@"completed"] boolValue])? UITableViewCellAccessoryCheckmark:UITableViewCellAccessoryNone;
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - Adding Item
+- (void) addNewitem:(UIBarButtonItem *)sender{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"New To-Do-Item" message:@"Enter the name of new Todo Item" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Add Item", nil ];
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alertView show];
+}
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if (buttonIndex!= alertView.cancelButtonIndex) {
+        UITextField *itemNameField = [alertView textFieldAtIndex:0];
+        NSString *itemName = itemNameField.text;
+        NSDictionary *itemToPut = @{@"name":itemName};
+        [self.items addObject:itemToPut];
+        [self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.items.count-1 inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
 }
 
 #pragma mark - UITableView Data Source Methods
@@ -41,9 +70,13 @@
     //    if (cell == nil) {
     //        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
     //    }
-    //    NSDictionary *item = _items[indexPath.row];
-    //    cell.textLabel.text = item[@"name"];
-    cell.textLabel.text = _items[indexPath.row];
+    NSDictionary *item = _items[indexPath.row];
+    cell.textLabel.text = item[@"name"];
+    if ([item[@"completed"] boolValue]) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else{
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
     return cell;
 }
 @end
