b37002e30c4ae9e38f565c7597dfe83dbf42a6b6
Admin <admin@Yashs-MacBook-Air.local>
2019-02-10 12:48:28 +0530

    Data represented in Table

--- Try/ViewController.m
+++ Try/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic) NSMutableArray *items;
 
 @end
 
@@ -17,7 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.items = @[@"item1",@"item2"].mutableCopy;
+    self.navigationItem.title=@"To-Do-List";
+    NSLog(@"viewdidload");
+
 }
 
-
+#pragma mark - UITableView Data Source Methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    NSLog(@"sections");
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSLog(@"Rows");
+    return self.items.count;
+}
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    static NSString *cellId = @"TodoListRow";
+    NSLog(@"inside cellForRowAtIndexpath");
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    //    if (cell == nil) {
+    //        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    //    }
+    //    NSDictionary *item = _items[indexPath.row];
+    //    cell.textLabel.text = item[@"name"];
+    cell.textLabel.text = _items[indexPath.row];
+    return cell;
+}
 @end
